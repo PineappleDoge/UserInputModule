7407b0ffc7ac808e2a270053e7af31c857383d16
PineappleDoge <PineappleDoge2020@gmail.com>
2021-11-03 22:12:18 -0400

    Update To File Directory

--- src/StarterPlayer/StarterPlayerScripts/LocalScript/init.client.lua
+++
@@ -1,9 +0,0 @@
-script:WaitForChild("UserInputModule"):WaitForChild("Mouse")
-
-local MouseModule = require(script.UserInputModule.Mouse)
-local MobileModule = require(script.UserInputModule.Mobile)
-local GamepadModule = require(script.UserInputModule.Gamepad)
-local KeyboardModule = require(script.UserInputModule.Keyboard)
-MouseModule:Init() 
-KeyboardModule:Init()
-KeyboardModule:Destroy()
